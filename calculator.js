6a6c827c8f91b120f28b987eba240d8eadc89314
vasupatel414 <patelvasu7562@gmail.com>
2024-06-09 17:41:06 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,10 @@
+let sumdata= (a,b)=>{
+return a+b;
+}
+let mindata = (a, b) => {
+    return a - b;
+}
+
+export let name="vasu";
+export {sumdata,mindata};
+// export default sumdata;
---
+++ index.js
@@ -0,0 +1,8 @@
+
+// import sumdata from "./calculator.js";
+import {sumdata as sum,mindata,name} from "./calculator.js";
+
+console.log(sum(10, 20))
+console.log(mindata(10, 20))
+console.log(name)
+
